4aa1699aaf11c2318196c0c2c03d7f8102c019d2
wasta <ccw5294@psu.edu>
2023-09-25 02:23:36 -0400

    Album class first draft and unit test created.

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -1,3 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Album implements Comparable<Album> {
+    private List<String> artists;
+    private int id;
+    private String title;
+    private int numSongs;
+
+    public Album(int id, List<String> artists, String title, int numSongs) {
+        this.id = id;
+        this.artists = artists;
+        this.title = title;
+        this.numSongs = numSongs;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<String> getArtists() {
+        return artists;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumSongs() {
+        return numSongs;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String artist : artists) {
+            stringBuilder.append(artist);
+            stringBuilder.append(", ");
+        }
+        if (stringBuilder.length() > 0) {
+            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+        }
+        return String.format("ID: " + id + " - - " + numSongs + " Songs - - [" + stringBuilder.toString() + "]");
+    }
+
+    @Override
+    public int compareTo(Album other) {
+        return this.id - other.id;
+    }
 
 }
